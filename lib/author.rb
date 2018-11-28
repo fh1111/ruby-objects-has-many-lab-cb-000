93cadf434ef08772541e4183d2ceaece16526ec6
Fer Hernandez <fh.1111@gmail.com>
2018-11-28 00:35:21 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,7 @@
 class Author
 
   attr_accessor :name
+  @@count = 0
 
   def initialize(name)
     @name = name
@@ -14,12 +15,18 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
+    @@count += 1
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
     post.author = self
+    @@count += 1
+  end
+
+  def self.post_count
+    
   end
 
 end
