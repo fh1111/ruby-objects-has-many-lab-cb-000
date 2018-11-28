88396f79814ef3ae91f517d8bd03b66569b8a46e
Fer Hernandez <fh.1111@gmail.com>
2018-11-28 00:04:29 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,7 @@
 class Artist
 
   attr_accessor :name
+  @@count = 0
 
   def initialize(name)
     @name = name
@@ -14,12 +15,17 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    @@count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
+    @@count += 1
+  end
+
+  def song_count
 
   end
 
